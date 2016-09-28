92f86868077f7f289da336f97822a5aa0c4ddf19
Rocketed Away <randy.wilander@gmail.com>
2016-09-27 22:31:47 -0700

    Fixed a bug in the UnionFind __init__.py file which was incorrectly defining __all__

--- data_structures/union_find/__init__.py
+++ data_structures/union_find/__init__.py
@@ -1,5 +1,6 @@
-__all__ = ["QuickFind, QuickUnion, WeightedQuickUnion"]
+__all__ = ["QuickFind", "QuickUnion", "WeightedQuickUnion", "WeightedQuickUnionWithPathCompression"]
 
 from quick_find import QuickFind
 from quick_union import QuickUnion
 from quick_union_weighted import WeightedQuickUnion
+from quick_union_weighted_pc import WeightedQuickUnionWithPathCompression
